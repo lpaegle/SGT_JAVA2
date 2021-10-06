52f1abe3dd1a85c7bbbe6a307b316fe2b387ab75
lpaegle <livijapaegle@gmail.com>
2021-10-06 21:50:49 +0300

    Fundamentals - lesson 2

---
+++ src/lesson1/helloWorldLibrary/Fundamentals.java
@@ -0,0 +1,34 @@
+package lesson1.helloWorldLibrary;
+
+public class Fundamentals {
+
+    public static void main(String[] args) {
+
+//        COMMENTS
+
+        // One line comments
+        /*
+        Multi-line comment or
+        block of comments
+         */
+        /**
+         * Documentation comments as JavaDoc format.
+         */
+
+    }
+
+    /**
+     * Every public method should be documented like this!
+     * <p>
+     * Comment for method with auto generated parameteres -
+     *
+     * @param param1
+     * @param param2
+     * @return
+     */
+    public String someMethod(String param1, int param2) {
+
+
+        return "Something";
+    }
+}
